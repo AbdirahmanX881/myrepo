9eb282be5f72e9581752410de6801d804fd73ece
abdirahman <abdirahman@kali.boss12>
2024-03-02 22:26:54 -0500

    version1

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let name = 'name of the student';
+let Name = 'sucdi'; 
+console.log(name);
+console.log(Name);
+
+// let cannot be a reserbed keyword
+// should be meaningful
+// cannot start with a number 
+// cannot contain a space or hyphen
+// case sensitive
+
+let firstName = 'mosh' , lastName = 'hamedani';
+console.log(firstName,lastName)
+
+//constant
+const interestRate = 0.3;
+console.log(interestRate);
+let isApprobed = true;
+console.log(isApprobed);
+let color = null;
+// undefined , string , number
+// javascript is dynamic
+
+ 
